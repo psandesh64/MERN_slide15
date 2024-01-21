4b108ef85f8d0738cf2d74bebb00ddc33766b8c8
psandesh64 <psandesh64@gmail.com>
2024-01-21 22:31:34 +0545

    first commit

--- controller/user_router.js
+++ controller/user_router.js
@@ -14,7 +14,10 @@ router.get('/user',async (request, response, next) => {
 router.post('/user',async (request, response, next) => {
     try{
         const { username, name, password} = request.body
-
+        
+        if (password.length < 3) {
+            return response.status(400).json({ error: 'Password must be at least 3 characters long' });
+        }
         const saltRounds = 10
         const passwordHash = await bcrypt.hash(password,saltRounds)
 
